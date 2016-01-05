ee2116c5acf9c23f74147c1a2fb613d2e25fd099
Joey Hayes <bjoeyhayes@gmail.com>
2016-01-05 15:48:43 -0500

    setting up structure

---
+++ inc/foundation/foundation.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main Foundation 6 file for merging Foundation 6 with Genesis
+ *
+ * @since 0.1.0
+ * @package GenFound
+ */
+
+// Navigation Markup 
+// require( get_stylesheet_directory() . '/foundation/inc/navigation.php' );
+
+// Grid Markup 
+require( get_stylesheet_directory() . '/foundation/inc/grid.php' );
+
+// Block Grid Markup 
+// require( get_stylesheet_directory() . '/foundation/inc/block-grid.php' );
+
+// Comments Markup
+// require( get_stylesheet_directory() . '/foundation/inc/forms.php' );
