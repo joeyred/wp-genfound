afe2571a7c4bb0e229a563e5413f8003784fcc84
Joey Hayes <bjoeyhayes@gmail.com>
2016-01-12 18:13:30 -0500

    new config setup

--- inc/foundation/config/grid-config-float.php
+++ inc/foundation/config/grid-config-float.php
@@ -13,16 +13,42 @@ function genfound_grid_config_settings() {
 
 		'header' => array( // Genesis Header
 			'site-header' 		 => 'row',
-			'title-area' 		 => 'small-12 medium-6 large-5 columns',
-			'header-widget-area' => 'small-12 medium-6 large-7 columns'
+			'title-area' 		 => 'small-12 medium-6 columns',
+			'header-widget-area' => 'small-12 medium-6 columns'
 		),
 		// PAGE LAYOUTS
-		'content-sidebar' => array(),
-		'sidebar-content' => array(),
-		'content-sidebar-sidebar' => array(),
-		'sidebar-sidebar-content' => array(),
-		'sidebar-content-sidebar' => array(),
-		'full-width-content' => array()
+		'content-sidebar' => array(
+			'content-sidebar-wrap' => 'row',
+			'content' 			   => 'large-9 columns',
+			'sidebar-primary' 	   => 'large-3 columns'
+		),
+		'sidebar-content' => array(
+			'content-sidebar-wrap' => 'row',
+			'content' 			   => 'large-9 large-push-3 columns',
+			'sidebar-primary' 	   => 'large-3 large-pull-9 columns'
+		),
+		'content-sidebar-sidebar' => array(
+			'content-sidebar-wrap' => 'row',
+			'content' 			   => 'large-7 columns',
+			'sidebar-primary' 	   => 'large-3 columns',
+			'sidebar-secondary'	   => 'large-2 columns'
+		),
+		'sidebar-sidebar-content' => array(
+			'content-sidebar-wrap' => 'row',
+			'content' 			   => 'large-7 large-push-5 columns',
+			'sidebar-primary' 	   => 'large-3 large-pull-7 columns',
+			'sidebar-secondary'	   => 'large-2 large-pull-10 columns'
+		),
+		'sidebar-content-sidebar' => array(
+			'content-sidebar-wrap' => 'row',
+			'content' 			   => 'large-7 large-push-3 columns',
+			'sidebar-primary' 	   => 'large-3 large-pull-7 columns',
+			'sidebar-secondary'	   => 'large-2 columns'
+		),
+		'full-width-content' => array(
+			'content-sidebar-wrap' => 'row',
+			'content' 			   => 'small-12 columns',
+		)
 	);
 
 	return $classes;
