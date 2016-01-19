899d6944a93154aa1a67c049fd742633c5acfaa9
Joey Hayes <bjoeyhayes@gmail.com>
2016-01-18 19:04:09 -0500

    cleaned things up a bit

--- inc/foundation/functions/menu.php
+++ inc/foundation/functions/menu.php
@@ -5,42 +5,39 @@
  * @package GenFound/Markup
  */
 
-function genfound_menu_config() {
-
-	$menu = array(
-		'bar' => array(
-			'title-bar' => '',
-			'top-bar' => ''
-		),
-		'container' => array(),
-		'menu' => array(), 
-		);
-}
-
-function genfound_nav_menu( $args ) {
-
-}
-
-function genfound_menu_component( $component, $orientation, $responsive_ats ) {
+/**
+ * Build foundation menu component and pass through `wp_nav_menu()`.
+ *
+ * Supported values for `$component` are:
+ *
+ *  - `drilldown`,
+ *  - `accordion`,
+ *  - `dropdown`.
+ *
+ * @param  string $component      the name of the foundation component the function should build.
+ * @param  string $theme_location registered theme menu to output.
+ * @return void
+ */
+function genfound_menu_component( $component, $theme_location ) {
 
 	$args = array(
-		'theme_location' => 'test',
-		'menu' => '',
-		'container' => false,
-		'container_class' => '',
-		'container_id' => '',
-		'menu_class' => 'menu',
-		'menu_id' => '',
-		'echo' => true,
-		'fallback_cb' => false,
-		'before' => '',
-		'after' => '',
-		'link_before' => '',
-		'link_after' => '',
-		'items_wrap' => '',
-		'depth' => 5,
-		'walker' => new Genfound_Menu_Walker(),
-		'submenu_class' => 'menu'
+		'theme_location'  	=> $theme_location,
+		'menu' 			  	=> '',
+		'container' 	  	=> false,
+		'container_class' 	=> '',
+		'container_id' 	  	=> '',
+		'menu_class' 		=> 'menu',
+		'menu_id' 			=> '',
+		'echo' 				=> true,
+		'fallback_cb' 		=> false,
+		'before' 			=> '',
+		'after' 			=> '',
+		'link_before' 		=> '',
+		'link_after' 		=> '',
+		'items_wrap' 		=> '',
+		'depth' 			=> 0,
+		'walker' 			=> new Genfound_Menu_Walker(),
+		'submenu_class' 	=> 'menu'
 	);
 
 	if ( $component === 'drilldown' ) {
@@ -49,22 +46,18 @@ function genfound_menu_component( $component, $orientation, $responsive_ats ) {
 		$args['menu_class'] .= ' vertical';
 		$args['submenu_class'] .= ' vertical';
 	}
+
 	if ( $component === 'accordion' ) {
 
 		$attributes = 'data-accordion-menu';
 		$args['menu_class'] .= ' vertical';
 		$args['submenu_class'] .= ' vertical nested';
-
 	}
+
 	if ( $component === 'dropdown' ) {
 
 		$attributes = 'data-dropdown-menu';
 		$args['menu_class'] .= ' dropdown';
-	}
-	if ( $component === 'responsive' ) {
-
-		$attributes = 'data-responsive-menu="' . $responsive_ats . '"';
-		$args['walker'] = new Genfound_Responsive_Menu_Walker();
 	}
 
 	$args['items_wrap'] = '<ul id="%1$s" class="%2$s" ' . $attributes . '>%3$s</ul>';
@@ -72,30 +65,28 @@ function genfound_menu_component( $component, $orientation, $responsive_ats ) {
     wp_nav_menu( $args );
 }
 
+/**
+ * Nav Menu Walker
+ */
 class Genfound_Menu_Walker extends Walker_Nav_Menu {
+	/**
+	 * Submenu markup genorator
+	 * @param  string  $output original output of walker class fucntion.
+	 * @param  integer $depth   determine how many submenus allowed in tree.
+	 * @param  array   $args    arguments from `wp_nav_menu( $args )`.
+	 * @return void
+	 */
 	 function start_lvl(&$output, $depth = 0, $args = array() ) {
         $indent = str_repeat("\t", $depth);
         $output .= "\n$indent<ul class=\"$args->submenu_class\">\n";
     }
 }
-class Genfound_Responsive_Menu_Walker extends Walker_Nav_Menu {
-	 function start_lvl(&$output, $depth = 0, $args = array() ) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "\n$indent<ul class=\"$args->walker_arg\">\n";
-    }
-}
 
-function genfound_drilldown_menu() {
 
-	?>
-	<ul class="menu">
-		<li><a href="#">One</a></li>
-		<li><a href="#">Two</a></li>
-		<li><a href="#">Three</a></li>
-		<li><a href="#">Four</a></li>
-	</ul>
-	<?php
-}
+
+
+
+
 
 
 
